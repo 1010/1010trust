eb47269ac70a219af254b7e21266b5c2c1a33256
andylolz <andy@1010uk.org>
2010-09-30 10:59:16 +0000

    Updated company and charity numbers

--- wp-content/themes/1010-trust/footer.php
+++ wp-content/themes/1010-trust/footer.php
@@ -12,8 +12,8 @@
 ?>
       <div id="footer">
         <div id="col1">
-          Charity number: 32154698<br />
-          Company number: 5465335
+          Charity number: 1137030<br />
+          Company number: 07223810
         </div>
         <div id="col2">
           8a Delancey Passage<br />
