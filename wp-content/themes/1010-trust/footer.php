af58e0fbcb0c96ceb1e75525552e7413e38d240f
andylolz <andy@1010uk.org>
2010-10-15 12:14:29 +0000

    Add terms and conditions to footer

--- wp-content/themes/1010-trust/footer.php
+++ wp-content/themes/1010-trust/footer.php
@@ -13,11 +13,13 @@
       <div id="footer">
         <div id="col1">
           Charity number: 1137030<br />
-          Company number: 07223810
+          Company number: 07223810<br />
+          <a href="<?php bloginfo( 'home' ); ?>/terms-and-conditions">Website terms of use</a>
         </div>
         <div id="col2">
           8a Delancey Passage<br />
-          London NW1 7NN
+          London<br />
+          NW1 7NN
         </div>
       </div><!-- #footer -->
     </div><!-- #wrapper -->
